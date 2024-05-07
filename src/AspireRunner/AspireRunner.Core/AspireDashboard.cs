8f47aae9441239db0b011895c00268ea93eda92f
Saif Alaqqad <saifo1999@gmail.com>
2024-05-07 13:40:13 +0300

    Add Core project

---
+++ src/AspireRunner/AspireRunner.Core/AspireDashboard.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace AspireRunner;
+
+public class AspireDashboard
+{
+    private readonly AspireDashboardOptions _options;
+    private readonly ILogger<AspireDashboard> _logger;
+
+    private Process? _process;
+
+    public AspireDashboard(IOptions<AspireDashboardOptions> options, ILogger<AspireDashboard> logger, Process? process)
+    {
+        _logger = logger;
+        _process = process;
+        _options = options.Value;
+    }
+}
---
+++ src/AspireRunner/AspireRunner.Core/AspireDashboardOptions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace AspireRunner;
+
+/// <summary>
+/// Configuration options used by the Aspire Dashboard.
+/// <see href="https://github.com/dotnet/aspire/tree/v8.0.0-preview.6.24214.1/src/Aspire.Dashboard/Configuration"/> 
+/// </summary>
+public class AspireDashboardOptions : IOptions<AspireDashboardOptions>
+{
+    public OtlpOptions Otlp { get; set; } = new OtlpOptions();
+
+    public FrontendOptions Frontend { get; set; } = new FrontendOptions();
+
+    public TelemetryLimitOptions? TelemetryLimits { get; set; }
+
+    public AspireDashboardOptions Value => this;
+}
+
+public class OtlpOptions
+{
+    public string? PrimaryApiKey { get; set; }
+
+    public string? SecondaryApiKey { get; set; }
+
+    public OtlpAuthMode? AuthMode { get; set; }
+
+    public string? EndpointUrl { get; set; }
+}
+
+public sealed class FrontendOptions
+{
+    public string? EndpointUrls { get; set; }
+
+    public FrontendAuthMode? AuthMode { get; set; }
+
+    public string? BrowserToken { get; set; }
+}
+
+public sealed class TelemetryLimitOptions
+{
+    public int MaxLogCount { get; set; } = 10_000;
+
+    public int MaxTraceCount { get; set; } = 10_000;
+
+    public int MaxMetricsCount { get; set; } = 50_000;
+
+    public int MaxAttributeCount { get; set; } = 128;
+
+    public int MaxAttributeLength { get; set; } = int.MaxValue;
+
+    public int MaxSpanEventCount { get; set; } = int.MaxValue;
+}
+
+public enum FrontendAuthMode
+{
+    Unsecured,
+    BrowserToken
+}
+
+public enum OtlpAuthMode
+{
+    Unsecured,
+    ApiKey
+}
