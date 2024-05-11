ab1f83e66c59eec538c179a5fc3a1279033efa65
Saif Alaqqad <saifo1999@gmail.com>
2024-05-11 10:38:01 +0300

    Refactor DotnetCli

--- src/AspireRunner/AspireRunner.Core/AspireDashboard.cs
+++ src/AspireRunner/AspireRunner.Core/AspireDashboard.cs
@@ -6,15 +6,59 @@ namespace AspireRunner;
 
 public class AspireDashboard
 {
+    private const string AspireSdkName = "Aspire.Dashboard.Sdk";
+    private const string AspireDashboardDll = "Aspire.Dashboard.dll";
+    private const string AspireDashboardInstanceFile = "aspire-dashboard.pid";
+
+    private readonly DotnetCli _dotnetCli;
     private readonly AspireDashboardOptions _options;
     private readonly ILogger<AspireDashboard> _logger;
 
     private Process? _process;
 
-    public AspireDashboard(IOptions<AspireDashboardOptions> options, ILogger<AspireDashboard> logger, Process? process)
+    public AspireDashboard(DotnetCli dotnetCli, IOptions<AspireDashboardOptions> options, ILogger<AspireDashboard> logger)
     {
         _logger = logger;
-        _process = process;
+        _dotnetCli = dotnetCli;
         _options = options.Value;
+    }
+
+    public bool IsInstalled()
+    {
+        return _dotnetCli is { SdkPath: not null } && _dotnetCli.GetInstalledWorkloads().Contains("aspire");
+    }
+
+    public void Start()
+    {
+        if (_process != null)
+        {
+            throw new ApplicationException("The Aspire Dashboard is already running.");
+        }
+
+        if (!IsInstalled())
+        {
+            throw new ApplicationException("The Aspire Dashboard is not installed.");
+        }
+
+        _logger.LogInformation("Starting the Aspire Dashboard...");
+
+        var packsFolder = _dotnetCli.GetPacksFolders()
+            .SelectMany(Directory.GetDirectories)
+            .First(dir => dir.Contains(AspireSdkName));
+
+        var newestVersionPath = Directory.GetDirectories(packsFolder)
+            .Select(d => new DirectoryInfo(d))
+            .MaxBy(d => new Version(d.Name))?
+            .FullName;
+
+        if (newestVersionPath == null)
+        {
+            throw new ApplicationException("Failed to find the newest version of the Aspire Dashboard.");
+        }
+        
+        var aspirePath = Path.Combine(newestVersionPath, "tools");
+        
+        // TODO: 
+
     }
 }
--- src/AspireRunner/AspireRunner.Core/DotnetCli.cs
+++ src/AspireRunner/AspireRunner.Core/DotnetCli.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 
 namespace AspireRunner;
 
@@ -13,25 +12,44 @@ public partial class DotnetCli
     public const string Executable = "dotnet";
 #endif
 
-    public string CliPath { get; }
+    public string CliPath { get; private init; } = null!;
 
-    public string DataPath { get; }
+    public string DataPath { get; private init; } = null!;
 
     public string? SdkPath { get; private set; }
 
-    private DotnetCli(string path)
+    private DotnetCli() { }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="DotnetCli"/> class.
+    /// </summary>
+    /// <returns>
+    /// A new instance of the <see cref="DotnetCli"/> class or null if the dotnet CLI wasn't found.
+    /// </returns>
+    public static DotnetCli? TryCreate()
     {
-        CliPath = path;
-        
-        var dataFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), DataFolderName);
-        if (!Directory.Exists(dataFolderPath))
+        var cliPath = GetCliPath();
+        if (cliPath == null)
         {
-            Directory.CreateDirectory(dataFolderPath);
+            return null;
         }
 
-        DataPath = dataFolderPath;
+        var cli = new DotnetCli
+        {
+            CliPath = cliPath,
+            DataPath = GetOrCreateDataPath()
+        };
+
+        cli.SdkPath = cli.GetSdkPath();
+        return cli;
     }
 
+    /// <summary>
+    /// Runs the dotnet CLI with the specified arguments, waits for completion and returns the full output.
+    /// </summary>
+    /// <param name="arguments">The arguments to pass to the dotnet CLI.</param>
+    /// <returns>The full output of the dotnet CLI.</returns>
+    /// <exception cref="InvalidOperationException">The dotnet CLI process failed to start.</exception>
     public string Run(string arguments)
     {
         var process = Process.Start(new ProcessStartInfo(Path.Combine(CliPath, Executable), arguments)
@@ -49,6 +67,16 @@ public partial class DotnetCli
         return process.StandardOutput.ReadToEnd();
     }
 
+    /// <summary>
+    /// Runs the dotnet CLI with the specified arguments.
+    /// </summary>
+    /// <param name="arguments">The arguments to pass to the dotnet CLI.</param>
+    /// <param name="workingDirectory">The working directory for the process.</param>
+    /// <param name="environement">The environment variables to set for the process.</param>
+    /// <param name="outputHandler">An action that receives the output of the process (stdout).</param>
+    /// <param name="errorHandler">An action that receives the error output of the process (stderr).</param>
+    /// <returns>The started process.</returns>
+    /// <exception cref="InvalidOperationException">The dotnet CLI process failed to start.</exception>
     public Process Run(string[] arguments, string? workingDirectory = null, IDictionary<string, string>? environement = null, Action<string>? outputHandler = null, Action<string>? errorHandler = null)
     {
         var processStartInfo = new ProcessStartInfo(Path.Combine(CliPath, Executable), arguments)
@@ -105,19 +133,10 @@ public partial class DotnetCli
         return process;
     }
 
-    public string? GetSdkPath()
-    {
-        var sdksOutput = Run("--list-sdks");
-        var sdks = sdksOutput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-
-        var sdkPath = sdks.Select(s => SdkOutputRegex().Match(s))
-            .Where(m => m.Success)
-            .Select(m => (Version: m.Groups[1].Value, Path: m.Groups[2].Value))
-            .MaxBy(s => new Version(s.Version)).Path;
-
-        return SdkPath = Path.GetDirectoryName(Path.TrimEndingDirectorySeparator(sdkPath));
-    }
-
+    /// <summary>
+    /// Returns all the dotnet packs folders available.
+    /// </summary>
+    /// <returns>An array containing the paths of all the dotnet packs folders available.</returns>
     public string[] GetPacksFolders()
     {
         var folders = new List<string>();
@@ -139,21 +158,71 @@ public partial class DotnetCli
 
         return folders.ToArray();
     }
-
-    public static DotnetCli? TryCreate()
+    
+    /// <summary>
+    /// Runs <c>dotnet workload list</c> and parses the output to get all installed workloads.
+    /// </summary>
+    /// <returns>An array containing all installed workloads.</returns>
+    public string[] GetInstalledWorkloads()
     {
-        var cliPath = GetCliPath();
-        if (cliPath == null)
+        var workloadsOutput = Run("workload list");
+        var workloadsMatch = TableContentRegex().Match(workloadsOutput);
+        if (!workloadsMatch.Success)
+        {
+            return [];
+        }
+
+        return workloadsMatch.Value
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(row => TableColumnSeperatorRegex().Split(row, 3)[0])
+            .Where(id => !string.IsNullOrWhiteSpace(id))
+            .ToArray();
+    }
+
+    /// <summary>
+    /// Returns the path of the latest SDK installed.
+    /// </summary>
+    /// <returns>The path of the latest SDK installed or null if no SDK was found.</returns>
+    private string? GetSdkPath()
+    {
+        var sdksOutput = Run("--list-sdks");
+        var sdks = sdksOutput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => SdkOutputRegex().Match(s))
+            .Where(m => m.Success)
+            .Select(m => (Version: m.Groups[1].Value, Path: m.Groups[2].Value))
+            .ToArray();
+
+        if (sdks.Length == 0)
         {
             return null;
         }
 
-        var cli = new DotnetCli(cliPath);
-        cli.GetSdkPath();
-
-        return cli;
+        var latestSdk = sdks.MaxBy(s => new Version(s.Version));
+        return Path.GetDirectoryName(Path.TrimEndingDirectorySeparator(latestSdk.Path));
     }
 
+    /// <summary>
+    /// Ensures the existence of the dotnet data folder (<c>~/.dotnet</c>.) and returns its path. 
+    /// </summary>
+    /// <returns>The path to the dotnet data folder.</returns>
+    private static string GetOrCreateDataPath()
+    {
+        var dataFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), DataFolderName);
+        if (!Directory.Exists(dataFolderPath))
+        {
+            Directory.CreateDirectory(dataFolderPath);
+        }
+
+        return dataFolderPath;
+    }
+
+    /// <summary>
+    /// Returns the path to the dotnet CLI.
+    /// <br/>
+    /// If the <c>DOTNET_HOST_PATH</c> environment variable is set, it will be used, otherwise the system's <c>PATH</c> will be checked for a dotnet CLI.
+    /// </summary>
+    /// <returns>The path to the dotnet CLI or null if it wasn't found.</returns>
+    /// <seealso href="https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-environment-variables#dotnet_host_path"/>
     private static string? GetCliPath()
     {
         var dotnetPath = Environment.GetEnvironmentVariable("DOTNET_HOST_PATH");
@@ -175,6 +244,11 @@ public partial class DotnetCli
         return null;
     }
 
+    /// <summary>
+    /// Returns all paths in the system's <c>PATH</c> environment variable.
+    /// </summary>
+    /// <returns>A string array containing all paths in the system's <c>PATH</c> environment variable.</returns>
+    /// <remarks>When running inside WSL, windows paths (like <c>/mnt/c/*</c>) will be excluded to avoid conflicts with windows dotnet installations</remarks>
     private static string[] GetEnvPath()
     {
         var pathEnv = Environment.GetEnvironmentVariable("PATH");
@@ -193,6 +267,11 @@ public partial class DotnetCli
         return paths;
     }
 
+
+    /// <summary>
+    /// Checks if the current process is running inside WSL.
+    /// </summary>
+    /// <returns>True if the current process is running inside WSL, false otherwise.</returns>
     private static bool IsRunningWsl()
     {
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
@@ -214,9 +293,6 @@ public partial class DotnetCli
         process.WaitForExit();
         var output = process.StandardOutput.ReadToEnd();
 
-        return output.Contains("Microsoft");
+        return output.Contains("microsoft-standard-WSL2", StringComparison.OrdinalIgnoreCase);
     }
-
-    [GeneratedRegex(@"([\d\.]+)\s+(?:\[(.+)\])?", RegexOptions.Compiled)]
-    private static partial Regex SdkOutputRegex();
 }
