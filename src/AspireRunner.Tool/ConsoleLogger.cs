4f14ed428f2b53014ffc72e9e609afe57901b6c1
Saif Alaqqad <saifo1999@gmail.com>
2024-05-25 10:36:30 +0300

    Add downloader options to AspireRunner.Tool

--- src/AspireRunner.Tool/ConsoleLogger.cs
+++ src/AspireRunner.Tool/ConsoleLogger.cs
@@ -4,15 +4,22 @@ namespace AspireRunner.Tool;
 
 public class ConsoleLogger<T> : ILogger<T>
 {
+    public bool Verbose { get; set; }
+
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
     {
+        if (!IsEnabled(logLevel))
+        {
+            return;
+        }
+
         var message = formatter(state, exception);
         if (string.IsNullOrWhiteSpace(message))
         {
             return;
         }
 
-        if (logLevel == LogLevel.Error)
+        if (logLevel == LogLevel.Error || exception is not null)
         {
             Console.Write(Bold().Red("Error "));
         }
@@ -20,11 +27,15 @@ public class ConsoleLogger<T> : ILogger<T>
         {
             Console.Write(Bold().Yellow("Warning "));
         }
+        else if (logLevel is LogLevel.Trace or LogLevel.Debug)
+        {
+            Console.Write(Bold().Cyan("Debug "));
+        }
 
         Console.WriteLine(message);
     }
 
-    public bool IsEnabled(LogLevel logLevel) => true;
+    public bool IsEnabled(LogLevel logLevel) => Verbose || logLevel is not (LogLevel.Debug or LogLevel.Trace);
 
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
 }
