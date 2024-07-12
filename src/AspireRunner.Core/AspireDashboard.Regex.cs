78f9b29c01e0b13bee4828c14e27f04fa17b36bd
Saif Alaqqad <saifo1999@gmail.com>
2024-07-12 23:48:54 +0300

    Use GeneratedRegex

--- src/AspireRunner.Core/AspireDashboard.Regex.cs
+++ src/AspireRunner.Core/AspireDashboard.Regex.cs
@@ -4,5 +4,13 @@ namespace AspireRunner.Core;
 
 public partial class AspireDashboard
 {
+#if NET8_0_OR_GREATER
+    private static readonly Regex LaunchUrlRegex = BuildLaunchUrlRegex();
+
+    [GeneratedRegex(@"((?:Login to the dashboard at)|(?:Now listening on:)) +(?<url>https?:\/\/[^\s]+)", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
+    private static partial Regex BuildLaunchUrlRegex();
+
+#else
     private static readonly Regex LaunchUrlRegex = new(@$"((?:{LoginConsoleMessage})|(?:{DashboardStartedConsoleMessage})) +(?<url>https?:\/\/[^\s]+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+#endif
 }
--- src/AspireRunner.Core/Helpers/DotnetCli.Regex.cs
+++ src/AspireRunner.Core/Helpers/DotnetCli.Regex.cs
@@ -4,6 +4,28 @@ namespace AspireRunner.Core.Helpers;
 
 public partial class DotnetCli
 {
+#if NET8_0_OR_GREATER
+    private static readonly Regex SdkOutputRegex = BuildSdkOutputRegex();
+
+    private static readonly Regex RuntimeOutputRegex = BuildRuntimeOutputRegex();
+
+    private static readonly Regex TableContentRegex = BuildTableContentRegex();
+
+    private static readonly Regex TableColumnSeperatorRegex = BuildTableColumnSeperatorRegex();
+
+    [GeneratedRegex(@"([\d\.\w\-]+?)\s+(?:\[(.+)\])?", RegexOptions.Compiled)]
+    private static partial Regex BuildSdkOutputRegex();
+
+    [GeneratedRegex(@"(.+?) ([\d\-_.\w]+?) \[(.+)\]", RegexOptions.Compiled)]
+    private static partial Regex BuildRuntimeOutputRegex();
+
+    [GeneratedRegex(@"-+\r?\n([\w\W]+?)\r?\n\r?\n", RegexOptions.Compiled)]
+    private static partial Regex BuildTableContentRegex();
+
+    [GeneratedRegex(@"\s{2,}", RegexOptions.Compiled)]
+    private static partial Regex BuildTableColumnSeperatorRegex();
+
+#else
     private static readonly Regex SdkOutputRegex = new(@"([\d\.\w\-]+?)\s+(?:\[(.+)\])?", RegexOptions.Compiled);
 
     private static readonly Regex RuntimeOutputRegex = new(@"(.+?) ([\d\-_.\w]+?) \[(.+)\]", RegexOptions.Compiled);
@@ -11,4 +33,5 @@ public partial class DotnetCli
     private static readonly Regex TableContentRegex = new(@"-+\r?\n([\w\W]+?)\r?\n\r?\n", RegexOptions.Compiled);
 
     private static readonly Regex TableColumnSeperatorRegex = new(@"\s{2,}", RegexOptions.Compiled);
+#endif
 }
