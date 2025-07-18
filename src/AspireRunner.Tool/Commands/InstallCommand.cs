09d3e85cb122d44b97929b32a72a78942eef53ab
Saif Alaqqad <saifo1999@gmail.com>
2025-07-18 20:32:33 +0300

    ummmm

--- src/AspireRunner.Tool/Commands/InstallCommand.cs
+++ src/AspireRunner.Tool/Commands/InstallCommand.cs
@@ -1,20 +1,10 @@
 ﻿namespace AspireRunner.Tool.Commands;
 
-public class InstallCommand : ICommand<InstallCommand.Settings>
+public class InstallCommand : AsyncCommand<InstallCommand.Settings>
 {
     public class Settings : CommandSettings { }
 
-    public Task<int> Execute(CommandContext context, Settings settings)
-    {
-        throw new NotImplementedException();
-    }
-
-    public ValidationResult Validate(CommandContext context, CommandSettings settings)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<int> Execute(CommandContext context, CommandSettings settings)
+    public override Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
         throw new NotImplementedException();
     }
---
+++ src/AspireRunner.Tool/Logging/InMemoryLogger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AspireRunner.Tool.Logging;
+
+public class InMemoryLogger<T>() : InMemoryLogger(typeof(T).Name), ILogger<T>;
+
+public class InMemoryLogger(string category) : ILogger
+{
+    private const int MaxLogLines = 2500;
+
+    private int _currentIndex;
+    private readonly LogMessage[] _log = new LogMessage[MaxLogLines];
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        var message = formatter(state, exception);
+        lock (_log)
+        {
+            _log[_currentIndex] = new LogMessage(category, logLevel, message);
+            _currentIndex = (_currentIndex + 1) % MaxLogLines;
+        }
+    }
+
+    public LogMessage[] CurrentLog => [.._log];
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
+}
